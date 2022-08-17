8cb84308c3c70fabddca7c871ea476b5a9629d9e
VictorCR18 <Cavalcantevictor2003@gmail.com>
2022-08-16 23:41:22 -0300

    Versao 3

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
+const checkbox = document.getElementsByClassName("switch--shadow")[0];
+const bg = document.querySelector('body');
+const audio = document.getElementById('audio');
+
 const theme = () => {
-    const checkbox = document.getElementsByClassName("switch--shadow")[0];
-    const bg = document.querySelector('body');
+    audio.play();
 
     if (checkbox.checked) {
         bg.style.backgroundColor = "#292C35";
     } else {
         bg.style.backgroundColor = "#F1F1F1";
     }
+
 }
 
